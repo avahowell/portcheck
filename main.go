1b05a9493ad4da76a585a486dcdd1e622b2d2780
Johnathan Howell <me@johnathanhowell.com>
2017-04-16 20:47:18 -0400

    use reqlimit package

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/johnathanhowell/reqlimit"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -15,15 +17,12 @@ type PortStatus struct {
 	Error string
 }
 
-var timeout = time.Second * 5
-
+// CheckPortHandler checks if the supplied address is connectable.
 func CheckPortHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(200)
-
 	st := PortStatus{}
 
-	conn, err := net.DialTimeout("tcp", ps.ByName("addr"), timeout)
+	conn, err := net.DialTimeout("tcp", ps.ByName("addr"), time.Second*10)
 	if err != nil {
 		st.Open = false
 		st.Error = err.Error()
@@ -42,5 +41,12 @@ func main() {
 	router := httprouter.New()
 	router.GET("/:addr", CheckPortHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// limit requests to prevent abuse
+	limitedRouter := reqlimit.New(router, 10, time.Minute)
+
+	listenAddr := flag.String("bind", ":8080", "address to listen on")
+	flag.Parse()
+
+	log.Println("portcheck is listening on", *listenAddr)
+	log.Fatal(http.ListenAndServe(*listenAddr, limitedRouter))
 }
